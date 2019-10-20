4d360758ed2c2fe7ea7529fdccbee31ea12c3fae
student <student@server1.example.com>
2019-10-20 15:02:34 +0530

    modified a script to create ec2 instance

--- ec2instancecreate.sh
+++ ec2instancecreate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-AMI="0b69ea66ff7391e80" 
-SUBNET_ID="1f5a6243"
-if aws ec2 run-instance --image-id $AMI --count 1 --instance-type t2.micro $SUBNET_ID
-    aws ec2 run-instance --image-id $AMI --count 1 --instance-type t2.micro $SUBNET_ID 
-fi
+AMI="ami-xxxxxxxx"
+SUBNET_ID="subnet-xxxxxxxxx"
+COUNT=1
+  aws ec2 run-instances --image-id $AMI --count $COUNT --instance-type t2.micro --subnet-id $SUBNET_ID 
+
